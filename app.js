f8fe24176ccaf910ffda6454fb17070e4aaf4873
nakayamaemiliano <eminaka_97@hotmail.com>
2025-08-12 18:15:13 -0300

    desafio completado

---
+++ app.js
@@ -0,0 +1,47 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo(){
+  const input = document.getElementById("amigo");
+  const nombre = input.value.trim(); 
+
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    amigos.push(nombre);
+  }
+
+  input.value = "";
+
+ 
+}
+
+
+function mostrarAmigos() {
+
+  const lista = document.getElementById("listaAmigos");
+
+  lista.innerHTML = "";
+
+  
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+
+function sortearAmigo() {
+  
+  if (amigos.length === 0) {
+    document.getElementById("resultado").innerHTML = "<li>No hay amigos para sortear</li>";
+    return;
+  }
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  document.getElementById("resultado").innerHTML = `<li>${amigoSorteado}</li>`;
+}
